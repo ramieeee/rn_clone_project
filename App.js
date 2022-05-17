5c6aa7801e0bc0387fa13759aa217fb28c326ef4
ramieeee <rambotty@gmail.com>
2022-05-17 21:45:09 +0900

    Add camera function

--- App.js
+++ App.js
@@ -31,6 +31,7 @@ import LandingScreen from './components/auth/Landing';
 import RegisterScreen from './components/auth/Register';
 import LoginScreen from './components/auth/Login';
 import MainScreen from './components/Main';
+import AddScreen from './components/main/Add';
 
 const Stack = createStackNavigator();
 
@@ -85,6 +86,7 @@ export class App extends Component {
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Main">
             <Stack.Screen name="Main" component={MainScreen} options={{headerShown: false}}/>
+            <Stack.Screen name="Add" component={AddScreen}/>
           </Stack.Navigator>
         </NavigationContainer>
       </Provider>
--- components/Main.js
+++ components/Main.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchUser } from '../redux/actions/index';
 import FeedScreen from './main/Feed';
-import AddScreen from './main/Add';
 import ProfileScreen from './main/Profile';
 
-const Tab = createBottomTabNavigator();
+const EmptyScreen = () => {
+  return(null);
+}
+
+const Tab = createMaterialBottomTabNavigator();
 
 export class Main extends Component {
   componentDidMount() { // 컴포넌트를 생성하고 첫 렌더링이 끝났을때 호출
@@ -19,27 +22,33 @@ export class Main extends Component {
   render() {
     // 버튼 랜더링
     return (
-      <Tab.Navigator>
+      <Tab.Navigator initialRouteName="Feed" labeled={false}>
         <Tab.Screen name="Feed" component={FeedScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} /> // check more at vector icons page
-          )
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} /> // check more at vector icons page
+            )
           }}/>
-          <Tab.Screen name="Add" component={AddScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="plus-box" color={color} size={26} /> // check more at vector icons page
-          )
+        <Tab.Screen name="AddContainer" component={EmptyScreen} // name이 Add이면 다른곳으로 navigate할 가능성이 있음
+          listeners={({ navigation }) => ({ // event 발생시 실행되는 코드
+            tabPress: event => {
+              event.preventDefault();
+              navigation.navigate("Add");
+            }
+          })} 
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="plus-box" color={color} size={26} /> // check more at vector icons page
+            )
           }}/>
-          <Tab.Screen name="Profile" component={ProfileScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account-circle" color={color} size={26} /> // check more at vector icons page
-          )
+        <Tab.Screen name="Profile" component={ProfileScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="account-circle" color={color} size={26} /> // check more at vector icons page
+            )
           }}/>
       </Tab.Navigator>
     )
