4e33cd0a6cc2a3eab7f8474503b508c6988877a2
ramieeee <rambotty@gmail.com>
2022-05-18 20:55:02 +0900

    Complete taking photo function

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
 
   useEffect(() => {
@@ -12,6 +14,15 @@ export default function App() {
       setHasPermission(status === 'granted');
     })();
   }, []);
+
+  const takePicture = async () => {
+    // access the camera first
+    if(camera) {
+      const data = await camera.takePictureAsync(null);
+      // console.log(data.uri); // 카메라 촬영시 uri 데이터가 생성되므로 확인용으로 로그 기록
+      setImage(data.uri);
+    }
+  }
 
   if (hasPermission === null) {
     return <View />;
@@ -23,6 +34,7 @@ export default function App() {
     <View >
       <View style={styles.cameraContainer}>
         <Camera
+          ref={ref => setCamera(ref)}
           style={styles.fixedRatio}
           type={type}
           ratio={"1:1"}/>
@@ -37,6 +49,11 @@ export default function App() {
           );
         }}>
       </Button>
+
+      <Button title="Take Picture" onPress={() => takePicture()}/>
+      
+      {/* if image exists, it will show the image tag information */}
+      {image && <Image source={{uri: image}} style={{flex: 1}}/>}
     </View>
   );
 }
